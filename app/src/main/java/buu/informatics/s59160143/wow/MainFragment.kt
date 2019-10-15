93f6741cca729fc5a40ee07301f99da1bd03e6fa
Anantz <anankhaita55@gmail.com>
2019-10-15 22:58:35 +0700

    add about

--- app/src/main/java/buu/informatics/s59160143/wow/MainFragment.kt
+++ app/src/main/java/buu/informatics/s59160143/wow/MainFragment.kt
@@ -2,11 +2,11 @@ package buu.informatics.s59160143.wow
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import buu.informatics.s59160143.wow.databinding.FragmentMainBinding
 
 /**
@@ -21,7 +21,17 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentMainBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_main, container, false)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.option_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            view!!.findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
 
