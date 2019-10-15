a6fa6b68f7d9d58c8cecc1b270498085897e5052
Anantz <anankhaita55@gmail.com>
2019-10-16 00:04:54 +0700

    add nav menu about

--- app/src/main/java/buu/informatics/s59160143/wow/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160143/wow/MainActivity.kt
@@ -2,19 +2,26 @@ package buu.informatics.s59160143.wow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import buu.informatics.s59160143.wow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this,navController)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
