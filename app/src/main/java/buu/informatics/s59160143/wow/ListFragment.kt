1bd6ee338624ebc5631a4368a50488c93424538a
Anantz <anankhaita55@gmail.com>
2019-10-15 22:21:14 +0700

    nav

---
+++ app/src/main/java/buu/informatics/s59160143/wow/ListFragment.kt
@@ -0,0 +1,29 @@
+package buu.informatics.s59160143.wow
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160143.wow.databinding.FragmentListBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentListBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_list, container, false)
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/buu/informatics/s59160143/wow/MainFragment.kt
@@ -0,0 +1,28 @@
+package buu.informatics.s59160143.wow
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160143.wow.databinding.FragmentMainBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentMainBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_main, container, false)
+        return binding.root
+    }
+
+
+}
