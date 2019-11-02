93170f9c3c49d83d3a126ae39c585757a1eb9367
Anantz <anankhaita55@gmail.com>
2019-11-02 19:34:51 +0700

    add submit

---
+++ app/src/main/java/buu/informatics/s59160143/wow/AppConstants.kt
@@ -0,0 +1,6 @@
+package buu.informatics.s59160143.wow
+
+object AppConstants {
+    val TAKE_PHOTO_REQUEST: Int = 2
+    val PICK_PHOTO_REQUEST: Int = 1
+}
--- app/src/main/java/buu/informatics/s59160143/wow/InsertFragment.kt
+++ app/src/main/java/buu/informatics/s59160143/wow/InsertFragment.kt
@@ -1,71 +1,69 @@
 package buu.informatics.s59160143.wow
 
 
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import buu.informatics.s59160143.wow.database.PeopleDatabaseModel
 import buu.informatics.s59160143.wow.databinding.FragmentInsertBinding
+import buu.informatics.s59160143.wow.databinding.FragmentListBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 /**
  * A simple [Fragment] subclass.
  */
 class InsertFragment : Fragment() {
-    private val people: ArrayList<People> = ArrayList<People>()
-    private  lateinit var binding: FragmentInsertBinding
+    private lateinit var binding: FragmentInsertBinding
+    private lateinit var people: ViewModel
+    var pictureUrl = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_insert,container,false)
-        for (i in 0..2) {
-            people.add(i, People("", "", "","","",""))
-        }
-        binding.btnAdd.setOnClickListener {
-            clickAdd()
-        }
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_insert, container, false
+        )
+
         return binding.root
     }
-    fun clickAdd() {
-        var name = binding.nameEditText
-        var age = binding.ageEditText
-        var proportion = binding.proportionEdittext
-        var place = binding.placeEditText
-        var price = binding.priceEditText
-        var contact = binding.contactEditText
-
-//        name.text = null
-//        age.text = null
-//        name.text = null
-//        proportion.text = null
-//        place.text = null
-//        price.text = null
-//        contact.text = null
-
-
-        var showName = name.text
-        var showAge = age.text
-        var showProportion = proportion.text
-        var showPlace = place.text
-        var showPrice = price.text
-        var showContact = contact.text
-
+    private fun submit() {
         binding.btnAdd.setOnClickListener {
-            people.add(
-                0,
-                People(
-                    showName.toString(),
-                    showAge.toString(),
-                    showProportion.toString(),
-                    showPlace.toString(),
-                    showPrice.toString(),
-                    showContact.toString()
-                )
-            )
-            Log.i("test","${people.size}")
+            //uploadImage()
+            if (!binding.nameEditText.text.isEmpty() && !binding.ageEditText.text.isEmpty() && !binding.proportionEdittext.text.isEmpty()
+                && !binding.placeEditText.text.isEmpty() && !binding.priceEditText.text.isEmpty() && !binding.contactEditText.text.isEmpty()) {
+                Handler().postDelayed({
+                    people.insert(
+                        PeopleDatabaseModel(
+                            0,
+                            pictureUrl,
+                            binding.nameEditText.text.toString(),
+                            binding.ageEditText.text.toString(),
+                            binding.proportionEdittext.text.toString(),
+                            binding.placeEditText.text.toString(),
+                            binding.priceEditText.text.toString(),
+                            binding.contactEditText.text.toString()
+                        )
+                    )
+                    Toast.makeText(getActivity(), "บันทึกข้อมูลเรียบร้อย", Toast.LENGTH_LONG).show()
+                    view?.findNavController()
+                        ?.navigate(R.id.action_InsertFragment_to_mainFragment)
+                }, 2000)
+            } else {
+                Toast.makeText(getActivity(), "กรุณากรอกข้อมูลให้ครบถ้วน", Toast.LENGTH_LONG).show()
+            }
+
         }
     }
+
 }
